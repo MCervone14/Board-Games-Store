b2be56eb75b1868775bb35345cd44d747491a95a
Michael Cervone <MCervone14@gmail.com>
2024-07-29 22:03:52 -0500

    got rid of plaiceholder module

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-import withPlaiceholder from "@plaiceholder/next";
-
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
